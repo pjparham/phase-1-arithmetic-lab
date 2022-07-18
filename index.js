67a4aafbdd1a16d2de1d87cb9b6379256c6c8565
Parker Parham <parkerparham@gmail.com>
2022-07-18 15:45:42 -0400

    commit tree 59b5310b2e9c9011a5c6ad78c7e3c6ba90ba53a2

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+
+// function random() {
+//     return Math.floor(Math.random() * 10) + 1;
+// }
+var multiply = num1*num2
+var random = Math.floor(Math.random()* 10) + 1;
+
+
+const num4 = 22;
+const num3 = 6;
+var mod = num4 % num3;
+var max = Math.max(1, 2, 3, 5, 9, 20, 6, 2)
